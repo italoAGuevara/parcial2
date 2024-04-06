47e62728846faf76b048a3df7c61bf582ddd5940
Italo Guevara <alberto.villamil.1997@gmail.com>
2024-04-05 19:58:05 -0500

    Se agregan oopciones

--- app/src/main/java/com/example/solucionparcial2/MainActivity2.java
+++ app/src/main/java/com/example/solucionparcial2/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.solucionparcial2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+
+        Aspirante data = (Aspirante)intent.getSerializableExtra("aspirante");
     }
 }
